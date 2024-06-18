8be743e1f6620ddddb5a5d07d5008078ac867490
lispking <lispking@qq.com>
2024-06-18 23:39:35 +0800

    Support loadContract and connectWallet method

--- src/lib/deploy.ts
+++ src/lib/deploy.ts
@@ -2,19 +2,28 @@ import { defaultConnectors } from '@fuels/connectors';
 import { Contract, ContractFactory, Fuel } from 'fuels';
 
 export async function deploy(bytecode: string, abi: any): Promise<Contract> {
+    const wallet = await connectWallet();
+    const factory = new ContractFactory(bytecode, abi, wallet);
+    return await factory.deployContract();
+}
+
+export async function loadContract(contractId: string, abi: any): Promise<Contract> {
+    const wallet = await connectWallet();
+    return new Contract(contractId, abi, wallet);
+}
+
+export async function connectWallet() {
     const fuel = new Fuel({
         connectors: defaultConnectors({ devMode: true }),
     });
 
-    await fuel.connect();
+    if (!await fuel.hasConnector()) {
+        await fuel.connect();
+    }
     
     const account = await fuel.currentAccount();
     if (!account) {
         throw new Error("Current account not authorized for this connection!");
     }
-
-    const wallet = await fuel.getWallet(account);
-
-    const factory = new ContractFactory(bytecode, abi, wallet);
-    return await factory.deployContract();
+    return await fuel.getWallet(account);
 }
