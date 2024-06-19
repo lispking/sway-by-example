1693fb23097f54fc756801f9f95008a2402d7d1d
lispking <lispking@qq.com>
2024-06-19 20:24:21 +0800

    add control-flow-if example

---
+++ src/routes/(examples)/13-control-flow-if/+page.svelte
@@ -0,0 +1,46 @@
+<script lang="ts">
+  import { page } from '$app/stores';
+  import { getExample } from "$lib/helpers";
+  import store from "$lib/store";
+  import { deploy } from "$lib/deploy";
+  import type { Contract } from "fuels";
+
+  import enMarkdown from "./content.en.md?raw";
+  import zhMarkdown from "./content.zh.md?raw";
+
+  import swayCode from "./src/main.sw?raw";
+  import hexContent from './out/release/control-flow-if.hex?raw';
+  import abiContent from './out/release/control-flow-if-abi.json?raw';
+  import storageSlotsContent from './out/release/control-flow-if-storage_slots.json?raw';
+
+  const url = new URL($page.url.href);
+  const lang = url.searchParams.get('lang');
+
+  let contract: Contract;
+  let bytecode = '0x' + hexContent;
+  let abi = JSON.parse(abiContent);
+  const storageSlots = JSON.parse(storageSlotsContent);
+  const markdown = lang === 'zh' ? zhMarkdown : enMarkdown;
+
+  $store = {
+    markdown,
+    swayCode,
+    deploy: async () => {
+      contract = await deploy(bytecode, abi, storageSlots);
+      return [contract];
+    },
+    messages: {},
+    getters: {
+      "test_func(5)": async () => {
+        const { value } =  await contract.functions.test_func(5).get()
+        return value.toNumber();
+      },
+      "test_func(10)": async () => {
+        const { value } =  await contract.functions.test_func(10).get()
+        return value.toNumber();
+      },
+    },
+    prev: getExample(import.meta.url).prev,
+    next: getExample(import.meta.url).next,
+  };
+</script>
